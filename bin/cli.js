95ee3ba1bd068b3466290f674334be38836fb987
Jason Johnston <johnston@nationbuilder.com>
2016-01-15 10:28:33 -0800

    switch out commander with meow

--- bin/cli.js
+++ bin/cli.js
@@ -1,38 +1,39 @@
 #!/usr/bin/env node
 'use strict'
 const path = require('path');
-const _ = require('underscore');
-const program = require('commander');
 const bundle = require('../lib/bundle');
 const exit = require('../lib/exit');
+const meow = require('meow');
+
+const cli = meow(`
+  Usage
+    Requires adding build config in package.json
+    see https://github.com/jhnstn/bundbi#example for more details
+
+    $ bundbi <build>
+
+  Options
+    -w, --watch Watches for changes in source
+
+  Examples
+    $ bundbi app -w
+`, {
+    alias: {
+      w: 'watch'
+    }
+});
 
 let packageJson = require(path.resolve('./package.json'));
 
 if(!packageJson.browserify || !packageJson.browserify.build) {
-  exit('nothing to do: can\'t find the build config');
+  exit('nothing to do: can\'t find the build config',0);
 }
 
 let builds = packageJson.browserify.build;
-function bundleTarget(target,options) {
-  let targetBuild = builds[target];
-  if(!targetBuild) {
-    exit(`nothing to do for '${target}' bundle`);
-  }
-
-  let opts = _.pick(options, 'watch');
-  bundle(Object.assign(targetBuild, {target}),
-         Object.assign(packageJson.browserify, opts));
+let target = cli.input[0];
+let targetBuild = builds[target];
+if(!targetBuild) {
+  exit(`nothing to do for '${target}' bundle`,0);
 }
-
-program
-  .version(packageJson.version)
-  .arguments('<target>')
-  .option('-w, --watch', 'enable watch',false)
-  .action((target) => {
-    let targetBuild = builds[target];
-    if(!targetBuild) {
-      exit(`nothing to do for '${target}' bundle`);
-    }
-    bundle(Object.assign(targetBuild, {target}),
-           Object.assign(packageJson.browserify, {watch: program.watch}));
-  }).parse(process.argv);
+bundle(Object.assign({},targetBuild, {target}),
+       Object.assign({},packageJson.browserify, cli.flags));
